9626f4ad0276013bbe1bcfa1ea6cf8f23b7a2e4a
PrabhashDilhan <aompdakmeemena@gmail.com>
2025-03-18 16:51:50 +0530

    Delete helm.sh

--- helm.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Install Helm
-curl -fsSL https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
-
-# Update package lists and install unzip
-sudo apt-get install -y unzip
